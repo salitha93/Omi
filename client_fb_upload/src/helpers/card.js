2df63a176890f6df3f376219055550dfe22cf423
unknown <salitha12@gmail.com>
2021-10-03 09:47:52 +0530

    syncing with live verison

--- client_fb_upload/src/helpers/card.js
+++ client_fb_upload/src/helpers/card.js
@@ -1,7 +1,11 @@
 class Card {
     constructor(scene) {
+        this.extra = {value: ""};
         this.render = (x, y, sprite) => {
-            let card = scene.add.image(x, y, sprite).setScale(0.3*(window.innerWidth/1280), 0.3*(window.innerHeight/780)).setInteractive();
+            console.log("Renderd the card: " );
+            //let card = scene.add.image(x, y, sprite).setScale(0.1*scene.widthScale, 0.1*scene.heightScale).setInteractive();
+            let card = new ICard(scene, x, y, sprite, this.extra)
+            card.setScale(0.075*scene.widthScale, 0.075*scene.heightScale).setInteractive();
             scene.input.setDraggable(card);
             return card;
         }
